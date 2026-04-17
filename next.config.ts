4dcc238cee3c4517aedb2c6d14b844c9a1334e7f
Utkarsh Jaiswal <utkarsh@email.com>
2026-04-17 22:58:04 +0530

    Fix constants.ts and next config to definitively resolve 404 images on github pages

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,18 @@
 import type { NextConfig } from "next";
 
 const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let repo = "";
+if (isGithubActions) {
+  repo = "/utkarsh-portfolio";
+}
 
 const nextConfig: NextConfig = {
-  ...(isGithubActions ? { output: "export" } : {}),
+  ...(isGithubActions ? { output: "export", basePath: repo, assetPrefix: repo } : {}),
   images: {
     unoptimized: !!isGithubActions,
+  },
+  env: {
+    NEXT_PUBLIC_BASE_PATH: repo,
   },
 };
 
