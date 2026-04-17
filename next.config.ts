7ea0e2ea30d9f6ba2cc73187ec8e0288a681aff2
Utkarsh Jaiswal <utkarsh@email.com>
2026-04-17 22:55:02 +0530

    Fix next config to conditionally build for github actions only to support vercel

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
 const nextConfig: NextConfig = {
-  output: "export",
+  ...(isGithubActions ? { output: "export" } : {}),
   images: {
-    unoptimized: true,
+    unoptimized: !!isGithubActions,
   },
 };
 
